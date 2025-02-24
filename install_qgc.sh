30bb0d2dc9f555c72ec9cb49b989bfd88e08e1f2
Mohamed_Fathallah <mohamed.fathallah@etudiant-enit.utm.tn>
2025-02-24 11:58:55 +0100

    Update script to download AppImage from GitHub Releases

--- install_qgc.sh
+++ install_qgc.sh
@@ -5,19 +5,19 @@ set -e  # Stop script on error
 echo "Installing QGroundControl..."
 
 # Create necessary directories
-sudo mkdir -p /opt/qgroundcontrol
+sudo mkdir -p /opt/qgroundcontroltest
 
-# Download QGC AppImage (Replace URL with your GitHub link)
-wget -O /opt/qgroundcontrol/QGroundControl.AppImage "https://drive.google.com/file/d/1UlFmIThwqQDBNIilSsao-pB_si2Rd97z/view?usp=sharing"
+# Download QGC AppImage from GitHub Releases
+wget -O /opt/qgroundcontrol/QGroundControl.AppImage "https://github.com/natsumed/QGC-Installer/releases/latest/download/QGroundControl.AppImage"
 sudo chmod +x /opt/qgroundcontrol/QGroundControl.AppImage
 
-# Download the icon (Replace URL)
+# Download the icon
 wget -O /opt/qgroundcontrol/qgc-icon.png "https://github.com/natsumed/QGC-Installer/raw/main/qgc-icon.png"
 
 # Create the .desktop file
 cat <<EOF | sudo tee /usr/share/applications/qgroundcontrol.desktop
 [Desktop Entry]
-Name=QGroundControl
+Name=QGroundControltest
 Comment=Ground Control Station for MAVLink UAVs
 Exec=/opt/qgroundcontrol/QGroundControl.AppImage
 Icon=/opt/qgroundcontrol/qgc-icon.png
@@ -30,4 +30,3 @@ EOF
 update-desktop-database ~/.local/share/applications/
 
 echo "Installation complete! You can find QGroundControl in your applications menu."
-
